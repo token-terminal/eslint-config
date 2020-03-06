43529bb44ccf690c75b50488c9204f7e96031819
youzi <youridaamen@gmail.com>
2020-03-06 11:08:13 +0100

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "extends": ["standard", "standard-react", "prettier"],
+  "rules": {
+    "no-console": [
+      1,
+      { "allow": ["info", "warn", "error", "time", "timeEnd"] }
+    ],
+    "no-alert": 1,
+    "no-debugger": 1,
+    "react/prop-types": 0,
+    "react/jsx-handler-names": 0
+  }
+}
