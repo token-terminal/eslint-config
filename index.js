144b27ad866b6eab42ddfa2162bf214716ad7b60
Ward Peeters <ward@coding-tech.com>
2022-09-10 17:29:36 +0200

    feat: disable use-before-define for functions (#2)
    Functions are hoisted so it's safe to disable the check for functions

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ module.exports = {
     "react/prop-types": 0,
     "react/jsx-handler-names": 0,
     "react/jsx-curly-newline": 0,
+    "no-use-before-define": ["error", "nofunc"],
     "react/react-in-jsx-scope": 0,
-    "no-use-before-define": ["error", { "functions": true, "classes": true, "variables": true }],
     "@typescript-eslint/no-unused-vars": "error",
   },
 };
